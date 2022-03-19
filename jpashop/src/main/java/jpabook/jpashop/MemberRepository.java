d24b4f1c0c9f5e9bc8a784bd47b4ed6ced35c52d
Antaeseon <ants412@naver.com>
2022-03-20 00:33:33 +0900

    feat: h2 연결 후 testCode 작성

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop;
+
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    //저장을 하고 나면 리턴값은 웬만해서는 멤버를 안하는게 좋다 (command query 분리)
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+}
