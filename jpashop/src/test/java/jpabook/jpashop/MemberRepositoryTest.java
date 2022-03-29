f036cdcd707f09d94b48a8af5aa283303bb07b37
Antaeseon <ants412@naver.com>
2022-03-30 01:37:50 +0900

    feat: Item Category ManyToMany 설정

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,39 +1,39 @@
-package jpabook.jpashop;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.transaction.annotation.Transactional;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
-@SpringBootTest
-class MemberRepositoryTest {
-
-    @Autowired
-    MemberRepository memberRepository;
-
-
-    @Test
-    @Transactional
-    @Rollback(value = false)
-    void testMember() throws Exception {
-        //given
-        Member member = new Member();
-        member.setUsername("memberA");
-        //when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
-
-        //then
-        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
-        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        Assertions.assertThat(findMember).isEqualTo(member); // 1차 캐시에서  가져오는 개념이기 때문에 select 쿼리는 나가지 않는다.
-        System.out.println("findMember == member: " + (findMember == member));
-
-
-    }
-}
+//package jpabook.jpashop;
+//
+//import org.assertj.core.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.annotation.Rollback;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//
+//@SpringBootTest
+//class MemberRepositoryTest {
+//
+//    @Autowired
+//    MemberRepository memberRepository;
+//
+//
+//    @Test
+//    @Transactional
+//    @Rollback(value = false)
+//    void testMember() throws Exception {
+//        //given
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//        //when
+//        Long saveId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(saveId);
+//
+//        //then
+//        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+//        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        Assertions.assertThat(findMember).isEqualTo(member); // 1차 캐시에서  가져오는 개념이기 때문에 select 쿼리는 나가지 않는다.
+//        System.out.println("findMember == member: " + (findMember == member));
+//
+//
+//    }
+//}
