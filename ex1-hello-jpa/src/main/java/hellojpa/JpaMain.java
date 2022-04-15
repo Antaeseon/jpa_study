e6ca0e5b13ace1efd1963de757e16bbba8d09e0e
Antaeseon <ants412@naver.com>
2022-04-16 02:47:27 +0900

    feat: 영속성 컨텍스트 테스트

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -20,18 +20,48 @@ public class JpaMain {
 
 
             //비영속
-            Member member = new Member();
-            member.setId(100L);
-            member.setName("HelloJPA");
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            //영속 상태
+//            System.out.println("=== BEFORE ===");
+//            em.persist(member);
+//            System.out.println("=== AFTER ===");
+//
+//            Member findMember = em.find(Member.class, 101L);
+//
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
 
-            //영속 상태
-            System.out.println("=== BEFORE ===");
-            em.persist(member);
-            em.detach(member);
-            System.out.println("=== AFTER ===");
+
+            /**
+             * 같은것을 두번 조회할 때에는 1차캐시에서 가져온다
+             */
+//            Member findMember1 = em.find(Member.class, 101L);
+//            Member findMember2 = em.find(Member.class, 101L);
+//
+//            System.out.println("result = " + (findMember1 == findMember2));
+
+//            Member member1 = new Member(150L, "A");
+//            Member member2 = new Member(160L, "B");
+//            em.persist(member1);
+//            em.persist(member2);
+//
+//            System.out.println("===================");
+//            tx.commit();
+
+
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ");
+
+            if (member.getName().equals("ZZZZZ")) {
+                em.persist(member);
+            }
+
+            System.out.println("==========");
 
             tx.commit();
-
         } catch (Exception e) {
             tx.rollback();
         } finally {
