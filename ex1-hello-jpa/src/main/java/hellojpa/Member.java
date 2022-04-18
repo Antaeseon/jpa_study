9023bcb80befa04263d9d5909cc7161d14363229
Antaeseon <ants412@naver.com>
2022-04-19 00:26:46 +0900

    feat: Generation Type 확인

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,9 +4,13 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -16,87 +20,35 @@ import java.util.Date;
 import java.util.Optional;
 
 @Entity
+@SequenceGenerator(
+	name = "MEMBER_SEQ_GENERATOR",
+	sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+	initialValue = 1, allocationSize = 50)
 public class Member {
-    @Id
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE,
+		generator = "MEMBER_SEQ_GENERATOR")
+	private Long id;
 
-    @Column(name = "name", nullable = false)
-    private String username;
+	@Column(name = "name", nullable = false)
+	private String username;
 
-    private int age;
+	public Member() {
+	}
 
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
+	public Long getId() {
+		return id;
+	}
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
+	public String getUsername() {
+		return username;
+	}
 
-    private LocalDate testLocalDate;
-
-    private LocalDateTime testLocalDateTime;
-
-    @Lob
-    private String description;
-
-    public Member() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
