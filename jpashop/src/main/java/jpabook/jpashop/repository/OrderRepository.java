15192656b8509453461338fe867685c8061c1081
Antaeseon <ants412@naver.com>
2022-06-06 03:14:08 +0900

    feat: querydsl 적용

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package jpabook.jpashop.repository;
 
+import static jpabook.jpashop.domain.QMember.*;
+import static jpabook.jpashop.domain.QOrder.*;
+
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.QMember;
+import jpabook.jpashop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Repository;
@@ -14,11 +20,19 @@ import javax.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
 	private final EntityManager em;
+	private final JPAQueryFactory query;
+
+	public OrderRepository(EntityManager em) {
+		this.em = em;
+		this.query = new JPAQueryFactory(em);
+	}
 
 	public void save(Order order) {
 		em.persist(order);
@@ -90,6 +104,32 @@ public class OrderRepository {
 		return query.getResultList();
 	}
 
+	public List<Order> findAll(OrderSearch orderSearch) {
+
+
+
+		return query.select(order)
+			.from(order)
+			.join(order.member, member)
+			.where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+			.limit(1000)
+			.fetch();
+	}
+
+	private BooleanExpression nameLike(String memberName) {
+		if(StringUtils.hasText(memberName)){
+			return null;
+		}
+
+		return member.name.like(memberName);
+	}
+
+	private BooleanExpression statusEq(OrderStatus statusCond){
+		if (statusCond == null) {
+			return null;
+		}
+		return order.status.eq(statusCond);
+	}
 	public List<Order> findAllWithMemberDelivery() {
 		return em.createQuery(
 			"select o from Order o "
--- jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ jpashop/src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -34,7 +34,7 @@ public class MemberServiceTest {
         Long saveId = memberService.join(member);
 
         //then
-        assertEquals(member, memberRepository.findOne(saveId));
+        assertEquals(member, memberRepository.findById(saveId));
     }
 
     @Test
