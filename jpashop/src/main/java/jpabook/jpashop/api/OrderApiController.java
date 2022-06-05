3a91d4137fbe46179ff140e007984153c1659bc0
Antaeseon <ants412@naver.com>
2022-06-05 15:13:10 +0900

    feat: 주문 조회 V5

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -90,6 +90,12 @@ public class OrderApiController {
 		return orderQueryRepository.findOrderQueryDtos();
 	}
 
+	@GetMapping("/api/v5/orders")
+	public List<OrderQueryDto> ordersV5(){
+		return orderQueryRepository.findOrderQueryDto_optimization();
+	}
+
+
 
 	@Data
 	static class OrderDto{
