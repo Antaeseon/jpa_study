2a2222d4854cc8f101503d37728c225c173fa318
Antaeseon <ants412@naver.com>
2022-06-09 02:01:46 +0900

    feat: Query 이용해서 dto 조회

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
@@ -20,5 +21,17 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
 	//em.createquery를 이용하면 이런 문제를 잡지 못한다.
 	// @Query(name = "Member.findByUsername")
 	List<Member> findByUsername(@Param("username") String username); //namedparameter 넘어갈 때에는 반드시 @Param 설정 필요
+
+
+	//로딩 시점에 오류가 발생하는 엄청난 장점!!!
+	@Query("select m from Member m where m.username = :username and m.age = :age")
+	List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+	@Query("select m.username From Member m")
+	List<String> findUsernameList();
+
+	@Query("select new study.datajpa.dto.MemberDto(m.id,m.username,t.name) from Member m join m.team t")
+	List<MemberDto> findMemberDto();
+
 
 }
