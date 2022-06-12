8a35605571a79839bf249037369b45c3069e1b58
Antaeseon <ants412@naver.com>
2022-06-12 16:06:56 +0900

    feat: Entity 설계

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -18,6 +20,7 @@ import study.querydsl.entity.QHello;
 
 @SpringBootTest
 @Transactional
+// @Rollback(value = false)
 class QuerydslApplicationTests {
 
 
