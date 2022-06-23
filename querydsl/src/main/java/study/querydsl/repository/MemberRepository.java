30c3dc328157974e0c335050d6f81365d3fcde8d
Antaeseon <ants412@naver.com>
2022-06-23 11:02:18 +0900

    feat: QuerydslPredicateExecutor 추가

--- querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package study.querydsl.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import study.querydsl.entity.Member;
 
@@ -10,7 +11,8 @@ import study.querydsl.entity.Member;
 Entity 검색이나 핵심 비지니스로직은 Member Repository!
 다른거는 그냥 MemberQueryRepository같은 것을 class로 만들어서 사용하는 것도 하나의 방법이다!!
  */
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom,
+	QuerydslPredicateExecutor<Member> {
 
 	List<Member> findByUsername(String username);
 }
